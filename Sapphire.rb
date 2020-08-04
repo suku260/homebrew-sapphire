271e14466b9ddaacf17e8550304ce02432d418be
Jay Kumar <suku260@gmail.com>
2020-08-04 14:29:25 -0500

    Update Sapphire.rb

--- Sapphire.rb
+++ Sapphire.rb
@@ -1,33 +1,23 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sapphire < Formula
-  desc "Sapphire Programming Language"
-  homepage "http://www.SapphireProgrammingLang.info"
-  url "www.github.com/suku260:Sapphire"
-  version "0.5"
+  desc ""
+  homepage ""
+  url "http://www.github.com/suku260:Sapphire"
   sha256 ""
-  license "MIT"
+  license "bsd"
 
   # depends_on "cmake" => :build
 
   def install
-      bin.install "Sapphire"
-    end
-    system "make", "install"    # system "cmake", ".", *std_cmake_args
+    # ENV.deparallelize
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test Sapphire`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "false"
   end
 end
-
