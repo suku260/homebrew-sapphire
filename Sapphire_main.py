2dc9f0d23e2070386ea6870293c8630ceb454d96
suku260 <suku260@gmail.com>
2020-10-15 14:58:35 -0500

    reorganized dir

--- Sapphire_main.py
+++ Sapphire_main.py
@@ -1,10 +1,10 @@
+#!/usr/bin/env python3
 import os
 import sys
 sys.path.insert(0, "../..")
 
 tokens = (
-    'NAME','IN','NOT','ARRAY','INPUT','STRING','CLASS','BUILD','FUNCTION','AND','OR','NUMBER','LE','GE','EQ','NE','G','L','LPAREN','RPAREN','LBRACE','RBRACE','IF','WHILE','FOR'
-)
+    'NAME','ARRAY','ID','STRING','AND','OR','NUMBER','LE','GE','EQ','NE','G','L','LPAREN','RPAREN','LBRACE','RBRACE')
 
 literals = ['=', '+','.','-', '*', '/','^','%',',']
 
@@ -17,22 +17,22 @@ t_OR = r'\|\|'
 t_AND = r'&&'
 t_G=r'>'
 t_L=r'<'
-t_CLASS=r'class'
-t_BUILD=r'build'
-t_FUNCTION=r'function'
-t_FOR=r'for'
-t_IF=r'if'
-t_WHILE=r'while'
-t_IN=r'in'
-t_NOT=r'not'
 t_LBRACE=r'\{'
 t_RBRACE=r'\}'
 t_LPAREN=r'\('
-t_INPUT=r'input'
 t_RPAREN=r'\)'
+reserved= {
+'CLASS' : 'class',
+'BUILD' : 'build',
+'FUNCTION' : 'function',
+'FOR' : 'for',
+'IF' : 'if',
+'WHILE' : 'while',
+'IN' : 'in',
+'NOT' : 'not',
+'INPUT' : 'input'}
 
-
-
+tokens=tokens+(tuple(reserved.values()))
 t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 t_STRING = r'"[a-zA-Z_][a-zA-Z_0-9]*"' 
 def EOL(t):
@@ -60,7 +60,12 @@ def t_newline(t):
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-
+"""def t_ID(t):
+	r'[clasbuildfuntonrwhetp]'
+	if t.value in reserved:
+		t.type = reserved[ t.value ]
+		return t"""
+	
 # Build the lexer
 import ply.lex as lex
 lexer = lex.lex()
@@ -81,17 +86,26 @@ def p_statement_assign(p):
     'statement : NAME "=" expression'
     names[p[1]] = p[3]
 def p_input_assign(p):
-    'statement : NAME "=" INPUT LPAREN expression RPAREN'
+    'statement : NAME "=" input LPAREN expression RPAREN'
     x=input('\n')
     names[p[1]] = x
+def p_in_comparison(p):
+   	"expression : expression in expression"
+   	
+   	boolean3=False
+   	if p[1] in p[3]:
+   		boolean3=True
+   	else:
+   		boolean3=False
+   	p[0]=boolean3
 def p_if_loop(p):
-	'statement : IF LPAREN expression RPAREN LBRACE expression RBRACE'
+	'statement : if LPAREN expression RPAREN LBRACE expression RBRACE'
 	print(f" if {p[3]} is true then do {p[6]}")
 def p_while_loop(p):
-	'statement : WHILE LPAREN expression RPAREN LBRACE expression RBRACE'
+	'statement : while LPAREN expression RPAREN LBRACE expression RBRACE'
 	print(f" while {p[3]} is true then do {p[6]}")
 def p_for_loop(p):
-	'statement : FOR LPAREN expression RPAREN LBRACE expression RBRACE'
+	'statement : for LPAREN expression RPAREN LBRACE expression RBRACE'
 	print(f" for {p[3]} is true then do {p[6]}")
 def p_conditional_and(p):
     '''statement : expression L expression AND expression L expression
@@ -254,13 +268,13 @@ def p_conditional_or(p):
     	p[0]=False
     print(p[0])
 def p_function_def(p):
-    'statement : FUNCTION expression LPAREN expression RPAREN LBRACE expression RBRACE'
+    'statement : function expression LPAREN expression RPAREN LBRACE expression RBRACE'
     print("function def")
 def p_class_def(p):
-    'statement : CLASS expression LPAREN expression RPAREN LBRACE expression RBRACE'
+    'statement : class expression LPAREN expression RPAREN LBRACE expression RBRACE'
     print("class def")
 def p_build_def(p):
-    'statement : BUILD expression LPAREN expression RPAREN LBRACE expression RBRACE'
+    'statement : build expression LPAREN expression RPAREN LBRACE expression RBRACE'
     print("build def")
 def p_statement_expr(p):
     'statement : expression'
@@ -323,17 +337,6 @@ def p_expression_uminus(p):
 def p_expression_group(p):
     "expression : '(' expression ')'"
     p[0] = p[2]
-def p_in_comparison(p):
-   	"statement : expression IN expression"
-   	
-   	boolean3=False
-   	if p[1] in p[3]:
-   		boolean3=True
-   	else:
-   		boolean3=False
-   	p[0]=True
-   
-   	
 
 
 def p_expression_number(p):
